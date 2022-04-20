2ab2355d5df348893ff40612949309ba964a7ae6
hassanaftab93 <hassanaftab.pk@gmail.com>
2022-04-20 15:10:33 +0500

    Pushing Deployment Code

--- crawl.sh
+++ crawl.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
+# This is to make the python file executable
+chmod u+x main.py
+
 echo "Please Enter a Valid URL or Press CTRL+C to Exit: "
 echo "Example: www.cheatcc.com "
 read url_input 
 
 python ./main.py "$url_input"
-
-# This is to make the python file executable
-chmod u+x main.py
-
-# Calling the main.py file to start running
-./main.py
